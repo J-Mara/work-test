cd56ffdd76e9267f1ebd9872db0f07e046ca1ff1
J-Mara <jmaragakis20@stuy.edu>
2021-09-19 19:43:43 -0400

    initial version of HW 02

---
+++ HW_02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+  printf("Good News Everyone! %d more! \n", 6);
+
+  int x = 12;
+  char n = 'p';
+
+  printf("here is a variable test: %d\n", x);
+  printf("here is another: %c\n", n);
+
+  return 0;
+}
