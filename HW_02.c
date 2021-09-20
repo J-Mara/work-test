8bdf6be7fd3aa37886bb3d0b9e99cb54d9e531ea
J-Mara <jmaragakis20@stuy.edu>
2021-09-19 20:28:50 -0400

    more tests

--- HW_02.c
+++ HW_02.c
@@ -6,9 +6,18 @@ int main() {
 
   int x = 12;
   char n = 'p';
+  short s = 100000;
+  unsigned int y = 11;
+  double d = 12.593204824502964051987;
 
   printf("here is a variable test: %d\n", x);
   printf("here is another: %c\n", n);
+  printf("here is an unsigned: %d\n", y);
+  printf("short out of bounds test: %d\n", s);
+  printf("double time: %lf\n", d);
+  printf("double time with 15 sigfigs: %0.15lf\n", d);
+  printf("double time with more sigfigs than explicit digits: %0.1500lf\n", d);
+  
 
   return 0;
 }
